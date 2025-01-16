bfa3848a27bf4e1b2f6e96fde9e1bacdb5982bed
Gatera <vgatera@gmail.com>
2025-01-16 11:11:57 +0200

    added md header with styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import { SignInButton } from '@clerk/clerk-react'
 import React from 'react'
 import { Button } from './components/ui/button'
+import Header from './components/Header'
 
 function Home() {
   return (
     <div>
+      <Header />
       <SignInButton mode='modal' forceRedirectUrl='/'>
         <Button>Sign In</Button>
       </SignInButton>
