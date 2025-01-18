69964efd106c513d5fb6ecf7d06e6964233e6c1d
Gatera <vgatera@gmail.com>
2025-01-18 14:23:32 +0200

    feat: add search bar design

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+  import { Separator } from "@/components/ui/separator"
+  import { CiSearch } from "react-icons/ci";
+
+  
 
 function Search() {
   return (
-    <div>Search</div>
+    <div className='p-2 md:p-5 bg-white rounded-md md:rounded-full flex-col md:flex md:flex-row gap-10 px-5 items-center md:w-[80%] lg:w-[70%]'>
+        <Select>
+            <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
+                <SelectValue placeholder="Cars" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="light">New</SelectItem>
+                <SelectItem value="dark">Used</SelectItem>
+            </SelectContent>
+        </Select>
+        <Separator orientation="vertical" className="hidden md:block" />
+        <Select>
+            <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
+                <SelectValue placeholder="Make" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="System">System</SelectItem>
+            </SelectContent>
+        </Select>
+        <Separator orientation="vertical" className="hidden md:block" />
+        <Select>
+            <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
+                <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="System">System</SelectItem>
+            </SelectContent>
+        </Select>
+
+        <div>
+            <CiSearch className='text-[50px] bg-primary rounded-full p-3 text-white hover:scale-105 transition-all cursor-pointer' />
+        </div>
+    </div>
   )
 }
 
