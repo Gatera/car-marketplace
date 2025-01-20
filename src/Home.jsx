9b7474cece63e0c18575782beb44562746842882
Gatera <vgatera@gmail.com>
2025-01-20 12:08:54 +0200

    feat: add browse by type

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { Button } from './components/ui/button'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Category from './components/Category'
 
 function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <Category />
     </div>
   )
 }
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,8 +1,20 @@
+import Data from '@/shared/Data'
 import React from 'react'
 
 function Category() {
   return (
-    <div>Category</div>
+    <div className='mt-40'>
+        <h2 className='font-bold text-3xl text-center mb-6'>Browse by Type</h2>
+
+        <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6 px-20'>
+            {Data.Category.map((category, index) => (
+                <div className='border rounded-xl p-3 items-center flex flex-col cursor-pointer hover:shadow-md'>
+                    <img src={category.icon} alt={category.name} width={40} height={40} />
+                    <h2 className='mt-2'>{category.name}</h2>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
