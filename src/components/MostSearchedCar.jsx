66216be362e40ece52e5b547b5aa023b593b8a6a
Gatera <vgatera@gmail.com>
2025-01-22 11:24:57 +0200

    feat: update most searched cars

--- src/components/MostSearchedCar.jsx
+++ src/components/MostSearchedCar.jsx
@@ -1,17 +1,34 @@
 import FakeData from '@/shared/FakeData';
 import React from 'react'
 import CarItem from './CarItem';
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
 
 function MostSearchedCar() {
     console.log(FakeData.carList);
     
   return (
-    <div>
+    <div className='mx-24'>
         <h2 className='font-bold text-3xl text-center my-16'>Most Searched Cars</h2>
 
-        {FakeData.carList.map((car, index)=> (
-            <CarItem car={car} />
-        ))}
+        <Carousel>
+            <CarouselContent>
+                {FakeData.carList.map((car, index)=> (
+                    <CarouselItem className="md:basis-1/3 lg:basis-1/4">
+                        <CarItem car={car} />
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+
+        
     </div>
   )
 }
